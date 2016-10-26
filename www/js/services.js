8553f7c1536b568fa49b06c8f79a4977f13247e8
Gabriel Otero <otr.gabriel@gmail.com>
2016-10-26 09:29:19 -0200

    fix img issue

--- www/js/services.js
+++ www/js/services.js
@@ -8,9 +8,9 @@ angular.module('starter.services', [])
   {
     id: 1,
     name: 'Trauma',
-    steps: ["../img/trauma/trauma1.png", "../img/trauma/trauma2.png", 
-            "../img/trauma/trauma3.png", "../img/trauma/trauma4.png", 
-            "../img/trauma/trauma5.png", ],
+    steps: ["img/trauma/trauma1.png", "img/trauma/trauma2.png", 
+            "img/trauma/trauma3.png", "img/trauma/trauma4.png", 
+            "img/trauma/trauma5.png", ],
     quiz: {
             question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend tincidunt turpis nec viverra?",
             awnsers: ["Curabitur vel", "Felis eli", "Vivamus volutpat", "Lectus efficitur"],
@@ -20,9 +20,9 @@ angular.module('starter.services', [])
   {
     id: 2,
     name: 'Engasgo',
-    steps: ["../img/engasgo/engasgo1.png", "../img/engasgo/engasgo2.png", 
-            "../img/engasgo/engasgo3.png", "../img/engasgo/engasgo4.png", 
-            "../img/engasgo/engasgo5.png", ],
+    steps: ["img/engasgo/engasgo1.png", "img/engasgo/engasgo2.png", 
+            "img/engasgo/engasgo3.png", "img/engasgo/engasgo4.png", 
+            "img/engasgo/engasgo5.png", ],
     quiz: {
             question: "",
             awnsers: [""],
@@ -31,9 +31,9 @@ angular.module('starter.services', [])
   },
   {
     id: 3,
-    name: 'Convulção',
-    steps: ["../img/convulcao/convulcao1.png", "../img/convulcao/convulcao2.png", 
-            "../img/convulcao/convulcao3.png", "../img/convulcao/convulcao4.png"],
+    name: 'Convulsão',
+    steps: ["img/convulcao/convulcao1.png", "img/convulcao/convulcao2.png", 
+            "img/convulcao/convulcao3.png", "img/convulcao/convulcao4.png"],
     quiz: {
             question: "",
             awnsers: [""],
@@ -43,7 +43,7 @@ angular.module('starter.services', [])
   {
     id: 4,
     name: 'Desmaio',
-    steps: ["../img/desmaio/desmaio1.png", "../img/desmaio/desmaio2.png"],
+    steps: ["img/desmaio/desmaio1.png", "img/desmaio/desmaio2.png"],
     quiz: {
             question: "",
             awnsers: [""],
@@ -53,9 +53,9 @@ angular.module('starter.services', [])
   {
     id: 5,
     name: 'Parada Cardiorrespiratória',
-    steps: ["../img/parada/parada1.png", "../img/parada/parada2.png", 
-            "../img/parada/parada3.png", "../img/parada/parada4.png", 
-            "../img/parada/trauma5.png", "../img/parada/trauma6.png"],
+    steps: ["img/parada/parada1.png", "img/parada/parada2.png", 
+            "img/parada/parada3.png", "img/parada/parada4.png", 
+            "img/parada/trauma5.png", "img/parada/trauma6.png"],
     quiz: {
             question: "",
             awnsers: [""],
@@ -65,9 +65,9 @@ angular.module('starter.services', [])
   {
     id: 99,
     name: 'Sobre',
-    steps: ["../img/sobre/sobre1.png", "../img/sobre/sobre2.png", 
-            "../img/sobre/sobre3.png", "../img/sobre/sobre4.png", 
-            "../img/sobre/sobre5.png", ],
+    steps: ["img/sobre/sobre1.png", "img/sobre/sobre2.png", 
+            "img/sobre/sobre3.png", "img/sobre/sobre4.png", 
+            "img/sobre/sobre5.png", ],
   }];
 
   return {
